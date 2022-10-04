ad110c9c39060d49e3e6d881c4f6c08b578660c1
mmfink <mmfink@users.noreply.github.com>
2022-10-04 15:45:24 -0600

    initial draft code

---
+++ R/maxnet.coefficients.R
@@ -0,0 +1,35 @@
+#' @export
+maxnet.coefficients <- 
+  function(envnames, object) {
+    if('data.table' %in% rownames(installed.packages())) {
+      `%nchin%` <- Negate(data.table::`%chin%`)
+      outable <- data.table(input=envnames)
+      xtrafeatures <- names(object$betas) %nchin% outable[, input]
+      outable <- rbind(outable, names(object$betas[xtrafeatures]),
+                       use.names=FALSE)
+      setkey(outable, input)
+      bs <- which(outable[, input] %in% names(object$betas))
+      outable[bs, coeff := object$betas][!bs, coeff := 0]
+      
+      fmn <- which(names(object$featuremins) %in% outable[, input])
+      outable[, minval := object$featuremins[fmn]]
+      outable[, maxval := object$featuremaxs[fmn]]
+      outable[, penalty := object$penalty.factor[fmn]]
+      outable[, abscoef := abs(coeff)]
+    } else {
+      outable <- data.frame(input=envnames, stringsAsFactors = FALSE)
+      xfeatures <- which(names(object$betas) %in% outable$input)
+      outable$input <- rbind(outable$input, names(object$betas[!xfeatures]),
+                             use.names=FALSE)
+      bs <- which(outable$input %in% names(object$betas))
+      outable$coeff = rep(0, length(outable))
+      outable$coeff[bs] = object$betas[xfeatures]
+      fmn <- which(names(object$featuremins) %in% outable$input)
+      outable$minval = object$featuremins[fmn]
+      outable$maxval = object$featuremaxs[fmn]
+      outable$penalty = object$penalty.factor[fmn]
+      outable$abscoef = abs(coeff)
+    }
+    
+    return(outable)
+  }
