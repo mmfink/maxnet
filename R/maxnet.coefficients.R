8913d3d2d925bc18d04d0bc47f6a21349e7a35e4
mmfink <mmfink@users.noreply.github.com>
2023-05-22 12:17:53 -0600

    Add variable importance function
    Based on https://rdrr.io/github/peterbat1/fitMaxnet/src/R/varImportance.R

--- R/maxnet.coefficients.R
+++ R/maxnet.coefficients.R
@@ -4,21 +4,29 @@ maxnet.coefficients <-
   function(envnames, object) {
     if('data.table' %in% rownames(installed.packages())) {
       #' @import data.table
-      `%nchin%` <- Negate(data.table::`%chin%`)
+      #`%nchin%` <- Negate(data.table::`%chin%`)
       outable <- data.table::data.table(input=envnames)
-      xtrafeatures <- names(object$betas) %nchin% outable[, input]
-      outable <- rbind(outable, names(object$betas[xtrafeatures]),
-                       use.names=FALSE)
+      xtrafeatures <- setdiff(names(object$betas), outable$input)
+      outable <- rbind(outable, xtrafeatures, use.names=FALSE)
+      #xtrafeatures <- names(object$betas) %nchin% outable[, input]
+      #outable <- rbind(outable, names(object$betas[xtrafeatures]),
+      #                 use.names=FALSE)
       data.table::setkey(outable, input)
-      xtra <- which(outable[, input] %nchin% names(object$betas))
-      tempdf <- data.table::SJ(input = names(object$betas), coeff = object$betas)
-      outable <- tempdf[outable, on="input"][xtra, coeff := 0]
-      
-      fmn <- which(names(object$featuremins) %in% outable[, input])
-      outable[, minval := object$featuremins[fmn]]
-      outable[, maxval := object$featuremaxs[fmn]]
-      outable[, penalty := object$penalty.factor[fmn]]
-      outable[, abscoef := abs(coeff)]
+      #xtra <- which(outable[, input] %nchin% names(object$betas))
+      tempdf1 <- data.table::SJ(input = names(object$betas), coeff = object$betas)
+      outable <- tempdf1[outable, on="input"]
+      outable <- outable[is.na(coeff), coeff := 0]
+      tempdf2 <- data.table::SJ(input = names(object$featuremins), 
+                               minval = object$featuremins, 
+                               maxval = object$featuremaxs, 
+                               penalty = object$penalty.factor)
+      outable <- tempdf2[outable, on="input"][, abscoef := abs(coeff)]
+      # fmn <- which(names(object$featuremins) %in% outable[, input])
+      # outable[, minval := object$featuremins[fmn]]
+      # outable[, maxval := object$featuremaxs[fmn]]
+      # #outable[, devexp := object$dev.ratio[fmn]] * 100
+      # outable[, penalty := object$penalty.factor[fmn]]
+      # outable[, abscoef := abs(coeff)]
     } else {
       outable <- data.frame(input=envnames, stringsAsFactors = FALSE)
       xfeatures <- which(names(object$betas) %in% outable$input)
@@ -30,6 +38,7 @@ maxnet.coefficients <-
       fmn <- which(names(object$featuremins) %in% outable$input)
       outable$minval = object$featuremins[fmn]
       outable$maxval = object$featuremaxs[fmn]
+      #outable$devexp = object$dev.ratio[fmn] * 100
       outable$penalty = object$penalty.factor[fmn]
       outable$abscoef = abs(coeff)
     }
---
+++ R/maxnet.varImp.R
@@ -0,0 +1,52 @@
+#' Variable importance for a maxnet model
+#'
+#' Computes variable importance scores for all variables present in a maxnet model object. Scores are indicative and should be interpreted with caution as variable interaction is not considered in the computation of the scores.
+#' @export
+#' @param modobj Object of class maxnet
+#' @param resp_table Data.frame. Response table. First column is assumed to be 'response'
+#' @param numReplicates Integer. Number of permutations performed to compute importance.
+#'
+#' @details {
+#' The method used to compute variable importance follows that used in R packages \pkg{biomod2} and \pkg{ecospat}. First, model predictions are made for each row of the environmental data table (or 'response table') \emph{resp_table}. 
+#'
+#' For each variable in the maxnet model object, values for the variable are permuted between rows and a model prediction made for each row using the permuted or shuffled data table. The permutation is performed \emph{numReplicates} times for each variable.
+#'
+#' At each permutation, a Pearson correlation is computed between reference predictions and the predicted values from the shuffled table. The importance score is 1 - correlation coefficient.
+#'
+#' A vector of mean scores for each variable expressed as a percentage of the sum of all mean scores is returned.
+#' }
+#'
+#' @return A named vector of percent importance scores for the variables present in the maxnet model object sorted from highest to lowest.
+#' @references based on peterbat1/fitMaxnet varImportance.R [https://rdrr.io/github/peterbat1/fitMaxnet/src/R/varImportance.R]
+
+maxnet.varImp <- function(modobj, resp_table, numReplicates = 5) {
+  varList <- names(modobj$samplemeans)
+  importance <- vector("numeric", length(varList))
+  names(importance) <- varList
+  envData <- resp_table[, -1]
+  fullModelVals <- predict(modobj, envData)
+  
+  for(thisVar in varList)
+  {
+    correls <- vector("numeric", numReplicates)
+    origVals <- envData[[thisVar]]
+    
+    for (thisRep in 1:numReplicates)
+    {
+      permInd <- sample(1:nrow(envData), nrow(envData))
+      envData[[thisVar]] <- origVals[permInd]
+      newModelVals <- predict(modobj, envData)
+      correls[thisRep] <- cor(fullModelVals, newModelVals)
+    }
+    
+    # Re-instate original values for the variable
+    envData[, thisVar] <- origVals
+    
+    # Compute the mean correlation for this variable
+    importance[thisVar] <- mean(correls)
+  }
+  
+  importance <- 1 - importance
+  
+  return(round(100*importance/sum(importance), 2))
+}
